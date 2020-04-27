ea424b7928b90b708b4cb21f7862bfcaa4f9b1e5
Gosia <goganna14@gmail.com>
2020-04-27 02:40:38 +0200

    Training model

--- traffic_sign.py
+++ traffic_sign.py
@@ -66,3 +66,7 @@ model.add(Dense(43, activation='softmax'))
 #Compilation of the model
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+epochs = 15
+history = model.fit(X_train, y_train, batch_size=32, epochs=epochs, validation_data=(X_test, y_test))
+model.save("my_model.h5")
+
