15f47f6b68cda65f7685a715b3a7427ea89a27f9
Małgorzata Marzec <goganna14@gmail.com>
2020-05-04 13:14:54 +0200

    Testing accuracy on test dataset

--- traffic_sign.py
+++ traffic_sign.py
@@ -70,3 +70,26 @@ epochs = 15
 history = model.fit(X_train, y_train, batch_size=32, epochs=epochs, validation_data=(X_test, y_test))
 model.save("my_model.h5")
 
+
+#testing accuracy on test dataset
+from sklearn.metrics import accuracy_score
+
+y_test = pd.read_csv('Test.csv')
+
+labels = y_test["ClassId"].values
+imgs = y_test["Path"].values
+
+data=[]
+
+for img in imgs:
+    image = Image.open(img)
+    image = image.resize((30,30))
+    data.append(np.array(image))
+
+X_test=np.array(data)
+
+pred = model.predict_classes(X_test)
+
+#Accuracy with the test data
+from sklearn.metrics import accuracy_score
+print(accuracy_score(labels, pred))
